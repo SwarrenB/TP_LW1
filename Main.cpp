13e6a91ef234134736b115235fd41d3f930fec70
Dan <57754312+SwarrenB@users.noreply.github.com>
2021-10-08 01:10:57 +0400

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -1,47 +1,54 @@
 #include <stdlib.h>
 #include <iostream>
-#include <string>
-#include <limits.h>
+#include <iomanip>
+#include <math.h>
 #include <vector>
 #include "lib/DistanceBetweenPointsLib.h"
 using namespace std;
 
-int input_number(std::istream& input)
-{
-    int ans;
-    input >> ans;
-    while (!input || !(INT_MIN <= ans <= INT_MAX))
-    {
-        input.clear();
-        std::cout << "You trying to input an incorrect value.";
-        input >> ans;
+template<typename T>
+auto checkData();
+
+template<>
+auto checkData<int>() {
+    int number;
+    while (!(cin >> number) || cin.peek() != '\n') {
+        cin.clear();
+        cout << endl << "This is an incorrect value. PLease, try again -> ";
+        while (cin.get() != '\n');
     }
-    return ans;
+    return number;
 }
 
 int main()
 {
-    cout << "***Calculating minimal distance between points***" << std::endl;
-    vector<int> x;
-    vector<int> y;
+    cout << fixed << setprecision(3);
+    cout << "***Calculating distance between two points***" << std::endl;
+    //vector<int> x;
+    //vector<int> y;
     
-    double L = INT_MAX, L_fact;
+    double L = INT_MAX;
+    double L_fact;
     cout << "How many points does your graph contain? -> ";
-        int number = input_number(cin);
-    for (int i = 1; i <= number; i++)
+    int number = checkData<int>();
+    int x[number];
+    int y[number];
+    if (number == 1 || number == 0)
+        L = 0;
+    for (int i = 0; i < number; i++)
     {
-        cout << "Input X coordinate of " << i << " point -> ";
-        x.push_back(input_number(cin));
-        cout << "Input Y coordinate of " << i << " point -> ";
-        y.push_back(input_number(cin));
-        for (int j = 0; j < x.size(); i++)
+        cout << "Input X coordinate of " << i + 1 << " point -> ";
+        x[i] = checkData<int>();
+        cout << "Input Y coordinate of " << i + 1 << " point -> ";
+        y[i] = checkData<int>();
+        for (int j = 0; j < i; j++)
         {
-            L_fact = Calculation(x[j], y[j], x[x.size() - 1], y[y.size() - 1]);
-            if (L_fact < L)
+            L_fact = Calculation(x[j], y[j], x[i], y[i]);
+            if (L_fact < L && L_fact != 0)
                 L = L_fact;
         }
     }
-    cout << "Minimal distance between your points is: " << L << endl;
+    cout << "Minimal distance between your points is -> " << L << endl;
     system("pause");
     return 0;
 }
